bde40ecb43bed0bc88704505929447a7d4bb0197
Phan Dinh Dung <dungpd.nan@gmail.com>
2026-08-27 11:06:22 +0700

    fix: update github pages config and permissions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-//import ClusterMap from './components/ClusterMap';
 import ClusterMap from './ClusterMap';
+
 function App() {
   return (
-    <div>
-      {/* Component tự động lấy full màn hình nhờ default props (100vh) */}
       <ClusterMap />
-      
-      {/* Hoặc tuỳ chỉnh kích thước qua props */}
-      {/* <ClusterMap containerHeight="500px" containerWidth="80%" /> */}
-    </div>
   );
 }
 
